8ed1419ee8ad9993b7bf989b79002134c78556b1
sergei-trofimov <feast.st@gmail.com>
2021-08-01 12:20:00 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,41 @@
 'use strict';
+/* eslint-disable */
+const listUrl = 'https://mate-academy.github.io/phone-catalogue-static/api/';
+const detailsUrl = 'https://mate-academy.github.io/phone-catalogue-static/api/phones/';
+/* eslint-enable */
+const getPhones = (url) => {
+  return fetch(url + 'phones.json')
+    .then(response => response.json())
+    .then(data => {
+      const listOfPhonesId = data.map(phone => phone.id);
 
-// write your code here
+      getPhonesDetails(detailsUrl, listOfPhonesId);
+    })
+    .catch(() => {
+      setTimeout(() => (new Error('Some problems with server')), 5000);
+    });
+};
+
+const getPhonesDetails = (url, listOfId) => {
+  const listOfPhonesName = document.createElement('ul');
+
+  listOfId.forEach(id => {
+    return fetch(url + id + '.json')
+      .then(response => response.json())
+      .then(data => {
+        const phoneItemNames = document.createElement('li');
+
+        phoneItemNames.textContent = `${data.name}`;
+        listOfPhonesName.append(phoneItemNames);
+      })
+      .catch(() => {
+        setTimeout(() => (new Error('Some problems with server')), 5000);
+      });
+  });
+
+  const body = document.querySelector('body');
+
+  body.append(listOfPhonesName);
+};
+
+getPhones(listUrl);
